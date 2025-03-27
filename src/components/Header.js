129c1cb7cab11e1666acd9356000a2a9c1098cfa
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-03-27 18:21:58 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,22 @@
+// src/components/Header.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Inicio</Link></li>
+          <li><Link to="/about">Sobre Nosotros</Link></li>
+          <li><Link to="/services">Servicios</Link></li>
+          <li><Link to="/gallery">Galería</Link></li>
+          <li><Link to="/contact">Contacto</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
 
