d5b3656056794910172eed16b80ec5d6cf94db4f
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-03-27 19:15:17 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,10 @@
 npm install --save-dev gh-pages
+npm install 
+npm start
+npm run deploy
+git clone https://github.com/lucasmarinskiba/my-photo-studio-backend.git
+cd my-photo-studio-backend
+npm install
+node server.js
+
+
