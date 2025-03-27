ade4c783bd16d8ff968e3302ea7ea3293964ed42
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-03-27 18:22:44 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Gallery from './components/Gallery';
+import Contact from './pages/Contact';
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </main>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
 
